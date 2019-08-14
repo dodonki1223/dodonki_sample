104b6482653f168ef82c36ee2f260f6229aa7550
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-08-14 15:22:21 +0900

    first commit

---
+++ lib/dodonki_sample.rb
@@ -0,0 +1,6 @@
+require "dodonki_sample/version"
+
+module DodonkiSample
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dodonki_sample/version.rb
@@ -0,0 +1,3 @@
+module DodonkiSample
+  VERSION = "0.1.0"
+end
