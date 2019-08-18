40fd558ccd0081fbe5cc1c632cae2986bba034cf
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-08-19 08:37:04 +0900

    CircleCI実行確認

--- spec/dodonki_sample_spec.rb
+++ spec/dodonki_sample_spec.rb
@@ -2,7 +2,6 @@ RSpec.describe DodonkiSample do
   it "has a version number" do
     expect(DodonkiSample::VERSION).not_to be nil
   end
-
   # it "does something useful" do
   #   expect(false).to eq(true)
   # end
